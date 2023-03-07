307d00f46156ccb402af179760e754432d6bfd1e
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-03-06 20:24:06 -0500

    fix

--- Notation/ambrosebs_Go forward 4 bars in Notation, otherwise decrease pitch cursor one semitone.lua
+++ Notation/ambrosebs_Go forward 4 bars in Notation, otherwise decrease pitch cursor one semitone.lua
@@ -1,29 +1,7 @@
---[[
- * ReaScript Name: Go forward 4 bars in Notation, otherwise decrease pitch cursor one semitone (match file name without extension and author)
- * Description:
- * Instructions: Run
- * Screenshot URl:
- * Author:
- * Author URl:
- * Repository:
- * Repository URl:
- * File URl:
- * Licence: GPL v3
- * Forum Thread:
- * Forum Thread URl:
- * REAPER: 5.0
- * Extensions:
---]]
- 
---[[
- * Changelog:
- * v1.1 (2015-06-12)
-   # Modification
-   + Addition
-   - Deletion
- * v1.0 (2015-02-27)
-  + Initial Release
---]]
+-- @description Go forward 4 bars in Notation, otherwise decrease pitch cursor one semitone (match file name without extension and author)
+-- @author frenchy64
+-- @version 1.0
+-- @about
 
 function InMusicalNotation(editor)
   return 2 == reaper.MIDIEditor_GetMode(editor)
