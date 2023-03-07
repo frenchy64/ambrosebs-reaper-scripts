3d1921917b64f1cc9aba0fb0de6a2e55f2f6ffaf
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-03-07 14:50:48 -0500

    wip

--- reascript-test/test/reascript_test/core_test.clj
+++ reascript-test/test/reascript_test/core_test.clj
@@ -30,28 +30,28 @@
   {:name "D5 Enharmonic Drum Notation"
    :root "D4"
    :virtual-midi-keyboard-middle "D5"
-   :instruments {"CH" {:name "China"},
-                 "HH" {:name "Hi-Hat Half"},
-                 "SC" {:name "Snare Center"},
-                 "HO" {:name "Hi-Hat Open"},
-                 "C1" {:name "Crash High"},
-                 "T3" {:name "Mid Tom"},
-                 "K1" {:name "Kick 1"},
-                 "C2" {:name "Crash Medium"},
-                 "T2" {:name "High Tom"},
-                 "CB" {:name "Cowbell"},
-                 "RB" {:name "Ride Bell"},
-                 "K2" {:name "Kick 2"},
-                 "T5" {:name "Very Low Tom"},
-                 "HC" {:name "Hi-Hat Closed"},
-                 "HP" {:name "Hi-Hat Pedal"},
-                 "RE" {:name "Ride Edge"},
-                 "T4" {:name "Low Tom"},
-                 "SS" {:name "Snare Stick"},
-                 "RS" {:name "Snare Rim Shot"},
-                 "T1" {:name "High Floor Tom"},
-                 "SP" {:name "Splash"},
-                 "RM" {:name "Ride Middle"}}
+   :instruments {"C1" {:name "Crash High"},
+                  "C2" {:name "Crash Medium"},
+                  "CB" {:name "Cowbell"},
+                  "CH" {:name "China"},
+                  "HC" {:name "Hi-Hat Closed"},
+                  "HH" {:name "Hi-Hat Half"},
+                  "HO" {:name "Hi-Hat Open"},
+                  "HP" {:name "Hi-Hat Pedal"},
+                  "K1" {:name "Kick 1"},
+                  "K2" {:name "Kick 2"},
+                  "RB" {:name "Ride Bell"},
+                  "RE" {:name "Ride Edge"},
+                  "RM" {:name "Ride Middle"},
+                  "RS" {:name "Snare Rim Shot"},
+                  "SC" {:name "Snare Center"},
+                  "SP" {:name "Splash"},
+                  "SS" {:name "Snare Stick"},
+                  "T1" {:name "High Floor Tom"},
+                  "T2" {:name "High Tom"},
+                  "T3" {:name "Mid Tom"},
+                  "T4" {:name "Low Tom"},
+                  "T5" {:name "Very Low Tom"}}
    :notation-map {"D4" ["HP", "CB"]
                   "E4" ["K2"],
                   "F4" ["K1"],
@@ -66,17 +66,76 @@
                   "A5" ["C1", "SP"],
                   "B5" ["CH"]}})
 
+;; :printed-note + :accidental are redundant for readability
+;; {midi-note-number info}
 (def drum-notation-map1-solution
-  {62 {:instrument-id "HP",
-       :printed-note "D4"},
-   63 {:instrument-id "CB",
-       :printed-note "Db4"},
-   64 {:instrument-id "K2",
-       :printed-note "E4"},
-   65 {:instrument-id "K1",
-       :printed-note "F4"},
-   66 {:instrument-id "T5",
-       :printed-note "Gb4"}})
+  (into (sorted-map)
+        {62 {:instrument-id "HP",
+             :printed-note "D4"
+             :accidental "natural"},
+         63 {:instrument-id "CB",
+             :printed-note "Db4"
+             :accidental "flat"},
+         64 {:instrument-id "K2",
+             :printed-note "E4"
+             :accidental "natural"},
+         65 {:instrument-id "K1",
+             :printed-note "F4"
+             :accidental "natural"},
+         66 {:instrument-id "T5",
+             :printed-note "Gb4"
+             :accidental "flat"}
+         67 {:instrument-id "T4",
+             :printed-note "Abb4"
+             :accidental "doubleflat"}
+         69 {:instrument-id "T3"
+             :printed-note "Bbb4"
+             :accidental "doubleflat"}
+         70 {:instrument-id "SC"
+             :printed-note "Cbb5"
+             :accidental "doubleflat"}
+         71 {:instrument-id "SS"
+             :printed-note "Cb5"
+             :accidental "flat"}
+         72 {:instrument-id "SR"
+             :printed-note "C5"
+             :accidental "natural"}
+         73 {:instrument-id "T2"
+             :printed-note "Db5"
+             :accidental "flat"}
+         74 {:instrument-id "T1"
+             :printed-note "Ebb5"
+             :accidental "doubleflat"}
+         75 {:instrument-id "RB"
+             :printed-note "Fbb5"
+             :accidental "doubleflat"}
+         76 {:instrument-id "RE"
+             :printed-note "Fb5"
+             :accidental "flat"}
+         77 {:instrument-id "RM"
+             :printed-note "F5"
+             :accidental "natural"}
+         78 {:instrument-id "HH"
+             :printed-note "Gb5"
+             :accidental "flat"}
+         79 {:instrument-id "HC"
+             :printed-note "G5"
+             :accidental "natural"}
+         80 {:instrument-id "HO"
+             :printed-note "G#5"
+             :accidental "sharp"}
+         81 {:instrument-id "C2"
+             :printed-note "G##5"
+             :accidental "doublesharp"}
+         82 {:instrument-id "C1"
+             :printed-note "A#5"
+             :accidental "sharp"}
+         83 {:instrument-id "SP"
+             :printed-note "A##5"
+             :accidental "doublesharp"}
+         84 {:instrument-id "CH"
+             :printed-note "B#5"
+             :accidental "sharp"}}))
 
 (let [midi-note-numbers (mapv :midi-note-number drum-notation-map1-solution)]
   (assert (apply < midi-note-numbers) midi-note-numbers))
