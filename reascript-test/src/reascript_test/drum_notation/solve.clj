8da19ebd37a27cc33ae23826f27696295bde1f49
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-03-09 01:48:06 -0500

    [skip ci]

--- reascript-test/src/reascript_test/drum_notation/solve.clj
+++ reascript-test/src/reascript_test/drum_notation/solve.clj
@@ -1,6 +1,5 @@
 (ns reascript-test.drum-notation.solve
   (:require [reascript-test.drum-notation.rep :refer :all]
-            [clojure.math.combinatorics :as comb]
             [clojure.set :as set]))
 
 (defn enharmonic-midi-numbers
@@ -18,93 +17,50 @@
   (vec (range (max root (- notated 2))
               (+ notated 3))))
 
-(defn allocation? [v]
-  (and (map? v)
-       (every? midi-number? (keys v))
-       (every? instrument-id? (vals v))
-       (apply distinct? (vals v))))
+(defn solution-or-error? [m]
+  (and (map? m)
+       (case (:type m)
+         :solution (solution? (:solution m))
+         :error (let [{:keys [instrument-clashes message]} m]
+                  (and (string? message)
+                       (set? instrument-clashes)
+                       (seq instrument-clashes)
+                       (every? instrument-id? instrument-clashes)))
+         false)))
 
-(defn possible-allocations-for-staff-position
-  [previous-allocations root notated instrument-ids]
-  {:pre [(every? allocation? previous-allocations)
-         (midi-number? root)
-         (c-major-midi-number? notated)
-         (<= root notated) ;;TODO see note in enharmonic-midi-numbers
-         (every? instrument-id? instrument-ids)
-         (vector? instrument-ids)
-         (apply distinct? instrument-ids)]
-   :post [(every? allocation? %)]}
-  (let [;; heuristic: trim states that contain allocations that are impossible based on the previous note's possible allocations
-        impossible-allocations (if (seq previous-allocations)
-                                 ;; TODO if the NEXT allocation is very sparse, then don't bother adding doublesharp, it's probably useless
-                                 (let []
-                                   (-> ;; if the previous allocation always chooses a particular midi note, cull allocations that include that note
-                                       (apply set/intersection (map (comp set keys) previous-allocations))
-                                       ))
-                                 #{})
-        possible-midi-nums (remove impossible-allocations (enharmonic-midi-numbers root notated))
-        n-instruments (count instrument-ids)]
-    ;; heuristic: try and pack notes to the left first
-    (cond
-      ;; pack initial allocation to the left (if they fit at all)
-      (empty? previous-allocations) (cond-> [] 
-                                      (<= n-instruments (count possible-midi-nums))
-                                      (conj (zipmap possible-midi-nums instrument-ids)))
-      :else
-      (if-some [always-available (let [previous-min (apply min (mapcat keys previous-allocations))]
-                                   (when (= 1 n-instruments)
-                                     (some #(when (and (< previous-min %)
-                                                       (not-any? (fn [allocation]
-                                                                   (allocation %))
-                                                                 previous-allocations))
-                                              %)
-                                           possible-midi-nums)))]
-        ;; choose left-most slot that's always available and not less than the minimum of the previous allocation
-        [{always-available (first instrument-ids)}]
-        (into [] (keep (fn [ns]
-                         (zipmap ns instrument-ids)))
-              ;; heuristic: trim states where two instruments are interchanged for no reason
-              (comb/combinations
-                possible-midi-nums
-                n-instruments))))))
-
-(defn possible-allocations
-  [root num-cs]
-  {:pre [(midi-number? root)
-         (midi-number-constraints? num-cs)]
-   :post [(vector? %)
-          (every? (every-pred vector? #(every? allocation? %))
-                  %)]}
-  (reduce (fn [acc [n is]]
-            (conj acc
-                  (possible-allocations-for-staff-position (peek acc) root n is)))
-          [] num-cs))
-
-(defn find-solutions [root-coord str-cs]
+(defn find-solution [root-coord str-cs]
   {:pre [(midi-coord? root-coord)
-         (notation-constraints? str-cs)]}
+         (notation-constraints? str-cs)]
+   :post [(solution-or-error? %)]}
   (let [root-num (midi-coord->number root-coord)
         num-cs (coord-str-constraints->midi-number-constraints str-cs)
-        instrument-set (into #{} cat (vals num-cs))
-        instrument->notated-num (into {} (map (fn [[n is]]
-                                                (zipmap is (repeat n))))
-                                      num-cs)
-        possible-states (possible-allocations root-num num-cs)
-        all-states (apply comb/cartesian-product possible-states)
-        all-solutions (keep (fn [state]
-                              (when (apply distinct? (mapcat keys state))
-                                (into (sorted-map)
-                                      (mapcat (fn [allocation]
-                                                {:pre [(allocation? allocation)]}
-                                                (->> allocation
-                                                     (map (fn [[midi-num instrument-id]]
-                                                            {:pre [(instrument-id? instrument-id)
-                                                                   (midi-number? midi-num)]}
-                                                            {midi-num
-                                                             {:instrument-id instrument-id
-                                                              :accidental (accidental-relative-to
-                                                                            (instrument->notated-num instrument-id)
-                                                                            midi-num)}})))))
-                                      state)))
-                            all-states)]
-    all-solutions))
+        instrument->notated-num (mapcat (fn [[n is]]
+                                          (map vector is (repeat n)))
+                                        num-cs)]
+    (loop [solution (sorted-map)
+           [[id notated-num] & instrument->notated-num] instrument->notated-num]
+      (when (seq solution)
+        (assert (solution? solution) (pr-str solution)))
+      (assert (instrument-id? id) (pr-str id))
+      (assert (midi-number? notated-num))
+      (let [next-free-midi-num (if (seq solution)
+                                 (-> solution rseq first key inc)
+                                 root-num)]
+        (if-not (midi-number? next-free-midi-num)
+          {:type :error
+           :instrument-clashes #{id}
+           :message "Could not fit instrument into MIDI range"}
+          (let [allocated-midi-num (some #(when (<= next-free-midi-num %)
+                                            %)
+                                         (enharmonic-midi-numbers root-num notated-num))]
+            (if-not allocated-midi-num
+              {:type :error
+               :instrument-clashes #{id (-> solution rseq first val :instrument-id)}
+               :message "Insufficient room for instruments"})
+            (let [solution (assoc solution allocated-midi-num
+                                  {:instrument-id id
+                                   :accidental (accidental-relative-to notated-num allocated-midi-num)})]
+              (if instrument->notated-num
+                (recur solution instrument->notated-num)
+                {:type :solution
+                 :solution solution}))))))))
--- reascript-test/test/reascript_test/drum_notation/solve_test.clj
+++ reascript-test/test/reascript_test/drum_notation/solve_test.clj
@@ -16,206 +16,66 @@
          (sut/enharmonic-midi-numbers 60 60)))
   (is (thrown? AssertionError (sut/enharmonic-midi-numbers 61 60))))
 
-(deftest possible-allocations-test
-  (is (= [[{62 "HP"}]]
-         (sut/possible-allocations 62
-                                   (sorted-map 62 ["HP"]))))
-  (is (= [[{62 "HP", 63 "CB"}]]
-         (sut/possible-allocations 62
-                                   (sorted-map 62 ["HP" "CB"]))))
-  (is (= [[{62 "HP", 63 "CB"}]
-          [{64 "K2"}]]
-         (sut/possible-allocations 62
-                                   (sorted-map 62 ["HP" "CB"]
-                                               64 ["K2"]))))
-  (is (= [[{60 "L1", 61 "L2", 62 "HP", 63 "CB"}]
-          [{64 "K2"}]]
-         (sut/possible-allocations 60
-                                   (sorted-map 62 ["L1" "L2" "HP" "CB"]
-                                               64 ["K2"]))))
-  (is (= [[{62 "HP", 63 "CB"}]
-          [{64 "K2"}]
-          [{65 "K1"}]]
-         (sut/possible-allocations (midi-coord->number (:root gp8/drum-notation-map1))
-                                   (into (sorted-map)
-                                         (select-keys (coord-str-constraints->midi-number-constraints (:notation-map gp8/drum-notation-map1))
-                                                      (range 62 66))))))
-  (is (= [[{62 "HP", 63 "CB"}]
-          [{64 "K2"}]
-          [{65 "K1"}]
-          [{66 "T5"}]
-          [{67 "T4"}]
-          [{69 "T3"}]
-          [{70 "RS", 71 "SC", 72 "SS"}
-           {70 "RS", 71 "SC", 73 "SS"}
-           {70 "RS", 71 "SC", 74 "SS"}
-           {70 "RS", 72 "SC", 73 "SS"}
-           {70 "RS", 72 "SC", 74 "SS"}
-           {70 "RS", 73 "SC", 74 "SS"}
-           {71 "RS", 72 "SC", 73 "SS"}
-           {71 "RS", 72 "SC", 74 "SS"}
-           {71 "RS", 73 "SC", 74 "SS"}
-           {72 "RS", 73 "SC", 74 "SS"}]
-          [{75 "T2"}] ;;hmm too restrictive?
-          [{76 "T1"}]
-          ;;TODO 75 not possible 
-          [{75 "RM", 77 "RB", 78 "RE"}
-           {75 "RM", 77 "RB", 79 "RE"}
-           {75 "RM", 78 "RB", 79 "RE"}
-           {77 "RM", 78 "RB", 79 "RE"}]
-          [{77 "HC", 78 "HH", 79 "HO", 80 "C2"}
-           {77 "HC", 78 "HH", 79 "HO", 81 "C2"}
-           {77 "HC", 78 "HH", 80 "HO", 81 "C2"}
-           {77 "HC", 79 "HH", 80 "HO", 81 "C2"}
-           {78 "HC", 79 "HH", 80 "HO", 81 "C2"}]
-          [{79 "C1", 80 "SP"}
-           {79 "C1", 81 "SP"}
-           {79 "C1", 82 "SP"}
-           {79 "C1", 83 "SP"}
-           {80 "C1", 81 "SP"}
-           {80 "C1", 82 "SP"}
-           {80 "C1", 83 "SP"}
-           {81 "C1", 82 "SP"}
-           {81 "C1", 83 "SP"}
-           {82 "C1", 83 "SP"}]
-          [{84 "CH"}]]
-         (sut/possible-allocations (midi-coord->number (:root gp8/drum-notation-map1))
-                                   (coord-str-constraints->midi-number-constraints (:notation-map gp8/drum-notation-map1))))))
-
-(deftest find-solutions-test
-  (is (= [{62 {:instrument-id "HP", :accidental "natural"}, 63 {:instrument-id "CB", :accidental "sharp"}}
-          {62 {:instrument-id "HP", :accidental "natural"}, 64 {:instrument-id "CB", :accidental "doublesharp"}}
-          {63 {:instrument-id "HP", :accidental "sharp"}, 64 {:instrument-id "CB", :accidental "doublesharp"}}]
-         (sut/find-solutions
+(deftest find-solution-test
+  (is (= {:type :solution
+          :solution {62 {:instrument-id "HP", :accidental "natural"}, 63 {:instrument-id "CB", :accidental "sharp"}}}
+         (sut/find-solution
            {:midi-name "D"
             :octave 4}
            {"D4" ["HP", "CB"]})))
-  (is (= (mapv th/join-lines
-               [["_C4__________________________"
-                 "|  | |♮|♯|  |  | | | | | |  |"
-                 "|  | | |C|  |  | | | | | |  |"
-                 "|  | | |B|  |  | | | | | |  |"
-                 "|  |_| |_|  |  |_| |_| |_|  |"
-                 "|   |HP |   |   |   |   |   |"
-                 "|___|___|___|___|___|___|___|"]
-                ["_C4__________________________"
-                 "|  | |♮| |𝄪 |  | | | | | |  |"
-                 "|  | | | |  |  | | | | | |  |"
-                 "|  | | | |  |  | | | | | |  |"
-                 "|  |_| |_|  |  |_| |_| |_|  |"
-                 "|   |HP |CB |   |   |   |   |"
-                 "|___|___|___|___|___|___|___|"]
-                ["_C4__________________________"
-                 "|  | | |♯|𝄪 |  | | | | | |  |"
-                 "|  | | |H|  |  | | | | | |  |"
-                 "|  | | |P|  |  | | | | | |  |"
-                 "|  |_| |_|  |  |_| |_| |_|  |"
-                 "|   |   |CB |   |   |   |   |"
-                 "|___|___|___|___|___|___|___|"]])
-         (mapv pretty-solution
-               (sut/find-solutions
-                 {:midi-name "D"
-                  :octave 4}
-                 {"D4" ["HP", "CB"]}))))
-  ;;FIXME this is wrong. missing HP
-  (is (= {62 {:instrument-id "K2", :accidental "doubleflat"}
-          63 {:instrument-id "K1", :accidental "doubleflat"}
-          65 {:instrument-id "T5", :accidental "doubleflat"}
-          67 {:instrument-id "T4", :accidental "doubleflat"}
-          69 {:instrument-id "T3", :accidental "doubleflat"}
-          70 {:instrument-id "RS", :accidental "doubleflat"}
-          71 {:instrument-id "SC", :accidental "flat"}
-          72 {:instrument-id "T2", :accidental "doubleflat"}
-          74 {:instrument-id "T1", :accidental "doubleflat"}
-          75 {:instrument-id "RM", :accidental "doubleflat"}
-          76 {:instrument-id "RB", :accidental "flat"}
-          77 {:instrument-id "HC", :accidental "doubleflat"}
-          78 {:instrument-id "HH", :accidental "flat"}
-          79 {:instrument-id "C1", :accidental "doubleflat"}
-          80 {:instrument-id "SP", :accidental "flat"}
-          81 {:instrument-id "CH", :accidental "doubleflat"}}
-         (first
-           (sut/find-solutions
-             {:midi-name "D"
-              :octave 4}
-             (:notation-map gp8/drum-notation-map1))))))
-
-(deftest possible-allocations-for-staff-position-test
-  ;; root cuts off some options
-  (is (= [{60 "HP"
-           61 "CB"}
-          {60 "HP"
-           62 "CB"}
-          {61 "HP"
-           62 "CB"}]
-         (sut/possible-allocations-for-staff-position nil 60 60 ["HP" "CB"])
-         (sut/possible-allocations-for-staff-position [{60 "K1"} {61 "K1"}] 60 60 ["HP" "CB"])))
-  ;; root allows all respellings
-  (is (= [{58 "HP"}
-          {59 "HP"}
-          {60 "HP"}
-          {61 "HP"}
-          {62 "HP"}]
-         (sut/possible-allocations-for-staff-position nil 58 60 ["HP"])))
-  (is (= [{58 "HP"
-           59 "CB"}
-          {58 "HP"
-           60 "CB"}
-          {58 "HP"
-           61 "CB"}
-          {58 "HP"
-           62 "CB"}
-          {59 "HP"
-           60 "CB"}
-          {59 "HP"
-           61 "CB"}
-          {59 "HP"
-           62 "CB"}
-          {60 "HP"
-           61 "CB"}
-          {60 "HP"
-           62 "CB"}
-          {61 "HP"
-           62 "CB"}]
-         (sut/possible-allocations-for-staff-position nil 58 60 ["HP" "CB"])))
-  (is (= [{58 "HP"
-           59 "CB"
-           60 "K1"
-           61 "K2"}
-          {58 "HP"
-           59 "CB"
-           60 "K1"
-           62 "K2"}
-          {58 "HP"
-           59 "CB"
-           61 "K1"
-           62 "K2"}
-          {58 "HP"
-           60 "CB"
-           61 "K1"
-           62 "K2"}
-          {59 "HP"
-           60 "CB"
-           61 "K1"
-           62 "K2"}]
-         (sut/possible-allocations-for-staff-position nil 58 60 ["HP" "CB" "K1" "K2"])))
-  (is (= [{58 "HP"
-           59 "CB"
-           60 "K1"
-           61 "K2"
-           62 "T5"}]
-         (sut/possible-allocations-for-staff-position nil 58 60 ["HP" "CB" "K1" "K2" "T5"])))
-  ;; trim states based on previous allocations
-  (is (= [{59 "HP"}
-          {60 "HP"}
-          {61 "HP"}
-          {62 "HP"}]
-         (sut/possible-allocations-for-staff-position [{58 "CB"}] 58 60 ["HP"])
-         (sut/possible-allocations-for-staff-position [{58 "CB" 60 "K1"} {58 "CB" 61 "K1"}] 58 60 ["HP"])))
-  (is (= [{60 "HP"}
-          {61 "HP"}
-          {62 "HP"}]
-         (sut/possible-allocations-for-staff-position [{58 "CB" 59 "K1"}] 58 60 ["HP"])
-         (sut/possible-allocations-for-staff-position [{58 "CB" 59 "K1" 60 "K2"}
-                                                       {58 "CB" 59 "K1" 61 "K2"}]
-                                                      58 60 ["HP"]))))
+  (is (= (th/join-lines
+           ["_C4__________________________"
+            "|  | |♮|♯|  |  | | | | | |  |"
+            "|  | | |C|  |  | | | | | |  |"
+            "|  | | |B|  |  | | | | | |  |"
+            "|  |_| |_|  |  |_| |_| |_|  |"
+            "|   |HP |   |   |   |   |   |"
+            "|___|___|___|___|___|___|___|"])
+         (pretty-solution
+           (:solution
+             (sut/find-solution
+               {:midi-name "D"
+                :octave 4}
+               {"D4" ["HP", "CB"]})))))
+  (is (= {:type :solution,
+          :solution
+          {62 {:instrument-id "HP", :accidental "natural"},
+           63 {:instrument-id "CB", :accidental "sharp"},
+           64 {:instrument-id "K2", :accidental "natural"},
+           65 {:instrument-id "K1", :accidental "natural"},
+           66 {:instrument-id "T5", :accidental "flat"},
+           67 {:instrument-id "T4", :accidental "doubleflat"},
+           69 {:instrument-id "T3", :accidental "doubleflat"},
+           70 {:instrument-id "RS", :accidental "doubleflat"},
+           71 {:instrument-id "SC", :accidental "flat"},
+           72 {:instrument-id "SS", :accidental "natural"},
+           73 {:instrument-id "T2", :accidental "flat"},
+           74 {:instrument-id "T1", :accidental "doubleflat"},
+           75 {:instrument-id "RM", :accidental "doubleflat"},
+           76 {:instrument-id "RB", :accidental "flat"},
+           77 {:instrument-id "RE", :accidental "natural"},
+           78 {:instrument-id "HC", :accidental "flat"},
+           79 {:instrument-id "HH", :accidental "natural"},
+           80 {:instrument-id "HO", :accidental "sharp"},
+           81 {:instrument-id "C2", :accidental "doublesharp"},
+           82 {:instrument-id "C1", :accidental "sharp"},
+           83 {:instrument-id "SP", :accidental "doublesharp"},
+           84 {:instrument-id "CH", :accidental "sharp"}}}
+         (sut/find-solution
+           {:midi-name "D"
+            :octave 4}
+           (:notation-map gp8/drum-notation-map1))))
+  (is (= (th/join-lines
+           ["_C4__________________________C5__________________________C6__________________________"
+            "|  | |♮|♯|♮ |♮ |♭|𝄫| |𝄫|𝄫|♭ |♮ |♭|𝄫|𝄫|♭ |♮ |♭|♮|♯|𝄪|♯|𝄪 |♯ | | | |  |  | | | | | |  |"
+            "|  | | |C|  |  |T| | | |R|  |  |T| |R|  |  |H| |H| |C|  |  | | | |  |  | | | | | |  |"
+            "|  | | |B|  |  |5| | | |S|  |  |2| |M|  |  |C| |O| |1|  |  | | | |  |  | | | | | |  |"
+            "|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |  |_| |_| |_|  |"
+            "|   |HP |K2 |K1 |T4 |T3 |SC |SS |T1 |RB |RE |HH |C2 |SP |CH |   |   |   |   |   |   |"
+            "|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|"])
+         (pretty-solution
+           (:solution
+             (sut/find-solution
+               {:midi-name "D"
+                :octave 4}
+               (:notation-map gp8/drum-notation-map1)))))))
