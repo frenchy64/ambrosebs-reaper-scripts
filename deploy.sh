8a782c995752e3579df57befaf3c057ff83cfdaa
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-03-09 16:52:13 -0500

    wip

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+git diff --exit-code
+git diff --cached --exit-code
+
+./compile.clj
+
+if [ ! git diff --exit-code ]; then
+  git add .
+  git commit -m "regenerate"
+fi
+
+reapack-index --commit
